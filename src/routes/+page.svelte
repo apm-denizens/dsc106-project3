da97213e4b25bfcecc43522719406b2631310972
0eti <samuelzhang012@gmail.com>
2024-02-18 09:57:28 -0800

    filter system

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,25 @@
 
     onMount(async () => {
         let svg = d3.select("svg");
-        svg.attr("width", window.innerWidth);
-        svg.attr("height", window.innerHeight);
+        width = window.innerWidth;
+        height = window.innerHeight;
+        svg.attr("width", width);
+        svg.attr("height", height);
+        svg.attr("viewBox", `0 0 ${width} ${height}`).attr(
+            "preserveAspectRatio",
+            "xMidYMid meet"
+        ); // Adjust this value based on your needs
 
         const zoom = d3
             .zoom()
             .scaleExtent([1, 8]) // Limit zoom scale (min, max)
             .translateExtent([
                 [0, 0],
-                [window.innerWidth, window.innerHeight],
+                [width, height],
             ]) // Limit panning to the dimensions of the SVG
             .extent([
                 [0, 0],
-                [window.innerWidth, window.innerHeight],
+                [width, height],
             ]) // Set the viewport extent to the SVG dimensions
             .on("zoom", (event) => {
                 const { x, y, k } = event.transform; // Destructure the transform object to get translate and scale
@@ -28,6 +34,7 @@
                 );
             });
 
+        // @ts-ignore
         svg.call(zoom);
 
         var width = +svg.attr("width");
@@ -65,7 +72,7 @@
             console.log(map);
 
             // @ts-ignore
-            let mouseOver = function () {
+            let mouseOver = function (d: any) {
                 d3.selectAll(".Country") // reset all countries
                     .transition()
                     .duration(200)
@@ -77,6 +84,10 @@
                     .duration(200)
                     .style("opacity", 1)
                     .style("stroke", "black");
+                d3.select("#tooltip")
+                    .style("opacity", 1) // Show the tooltip
+                    // @ts-ignore
+                    .html(`Region: ${d.properties.name}<br>Value: ${d.value}`); // Set the tooltip content
             };
 
             let mouseLeave = function () {
@@ -85,6 +96,13 @@
                     .duration(200)
                     .style("opacity", 1)
                     .style("stroke", "transparent");
+                d3.select("#tooltip").style("opacity", 0); // Hide the tooltip
+            };
+
+            let mouseMove = function (event) {
+                d3.select("#tooltip")
+                    .style("left", event.pageX + 10 + "px") // Position the tooltip to the right of the cursor
+                    .style("top", event.pageY + 10 + "px"); // Position the tooltip below the cursor
             };
 
             projection.fitSize([width, height], geo_json);
@@ -107,8 +125,101 @@
                 .style("opacity", 1)
                 .on("mouseover", mouseOver)
                 .on("mouseleave", mouseLeave);
+
+            svg.selectAll("path");
         }
     });
+
+    $: filters = {
+        renewables: false,
+        renewable_types: {
+            solar: false,
+            wind: false,
+            hydro: false,
+            geothermal: false,
+            biomass: false,
+            nuclear: false,
+        },
+
+        fossil_fuels: false,
+        fossil_fuel_types: {
+            coal: false,
+            oil: false,
+            natural_gas: false,
+        },
+    };
+
+    const handleFilterInput = (type: "renewables" | "ffs") => {
+        if (type == "renewables") {
+            const setTo = !filters.renewables;
+            filters.renewables = setTo;
+            filters.renewable_types.solar = setTo;
+            filters.renewable_types.wind = setTo;
+            filters.renewable_types.hydro = setTo;
+            filters.renewable_types.geothermal = setTo;
+            filters.renewable_types.biomass = setTo;
+            filters.renewable_types.nuclear = setTo;
+        } else if (type == "ffs") {
+            const setTo = !filters.fossil_fuels;
+            filters.fossil_fuels = setTo;
+            filters.fossil_fuel_types.coal = setTo;
+            filters.fossil_fuel_types.oil = setTo;
+            filters.fossil_fuel_types.natural_gas = setTo;
+        }
+    };
+
+    function capitalizeFirstLetter(string: string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
 </script>
 
-<svg id="my_dataviz" width="800" height="600"></svg>
+<div>
+    <h1>Worldwide Energy Usage</h1>
+
+    <form>
+        <label>
+            <input
+                type="checkbox"
+                bind:checked={filters.renewables}
+                on:input={(e) => handleFilterInput("renewables")}
+            />
+            Renewables
+        </label>
+        <ul>
+            {#each Object.keys(filters.renewable_types) as key (key)}
+                <li>
+                    <label>
+                        <input
+                            type="checkbox"
+                            bind:checked={filters.renewable_types[key]}
+                        />
+                        {capitalizeFirstLetter(key)}
+                    </label>
+                </li>
+            {/each}
+        </ul>
+
+        <label>
+            <input
+                type="checkbox"
+                bind:checked={filters.fossil_fuels}
+                on:input={(e) => handleFilterInput("ffs")}
+            />
+            Fossil Fuels
+        </label>
+        <ul>
+            {#each Object.keys(filters.fossil_fuel_types) as key (key)}
+                <li>
+                    <label>
+                        <input
+                            type="checkbox"
+                            bind:checked={filters.fossil_fuel_types[key]}
+                        />
+                        {capitalizeFirstLetter(key)}
+                    </label>
+                </li>
+            {/each}
+        </ul>
+    </form>
+    <svg id="my_dataviz"></svg>
+</div>
